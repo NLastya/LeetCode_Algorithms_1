a4090c1037ea5857b0dcc66b0fae282f4864a11c
NLastya <nlastya1994@mail.ru>
2023-02-28 14:08:41 +0300

    first commit

---
+++ 1 Day 1.js
@@ -0,0 +1,19 @@
+class Solution {
+    public:
+        int search(vector<int>& nums, int target) {
+            StartInd = 0;
+            EndInd = nums.lenght-1;
+
+            while target > mums[Math.ceil(endInd/2)]{
+            start = nums.lenght/2;
+            end = nums.lenght;
+            }
+            elif {target = endInd/2}
+            else{
+                start = 0;
+                end = nums.lenght/2;
+            }
+            
+
+        }
+    };
